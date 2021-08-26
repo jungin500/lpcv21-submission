563b3693048c1baac19028599d5d1efda7356d2c
LimeOrangePie <ji5489@gmail.com>
2021-08-26 22:28:35 +0900

    [수정] Deepsort Feature Extractor 코드 가독성 수정

--- solution/deep_sort/deep_sort/deep/feature_extractor.py
+++ solution/deep_sort/deep_sort/deep/feature_extractor.py
@@ -9,42 +9,44 @@ from .model import Net
 class Extractor(object):
     def __init__(self, model_path, use_cuda=True):
         self.net = Net(reid=True)
-        self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
+        self.net.eval()
+        
+        self.device = torch.device("cuda") if torch.cuda.is_available() and use_cuda else torch.device("cpu")
         state_dict = torch.load(model_path, map_location=lambda storage, loc: storage)['net_dict']
         self.net.load_state_dict(state_dict)
         logger = logging.getLogger("root.tracker")
-        print("Loading weights from {}... Done!".format(model_path))
+        logger.info("Loading weights from {}... Done!".format(model_path))
         self.net.to(self.device)
+
         self.size = (64, 128)
-        self.norm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ])
+        self.means = np.expand_dims(np.expand_dims(np.array([0.485, 0.456, 0.406]), 0), 0)
+        self.stds = np.expand_dims(np.expand_dims(np.array([0.229, 0.224, 0.225]), 0), 0)
         
 
-
-    def _preprocess(self, im_crops):
-        """
-        TODO:
-            1. to float with scale from 0 to 1
-            2. resize to (64, 128) as Market1501 dataset did
-            3. concatenate to a numpy array
-            3. to torch Tensor
-            4. normalize
-        """
-        def _resize(im, size):
-            return cv2.resize(im.astype(np.float32)/255., size)
-
-        im_batch = torch.cat([self.norm(_resize(im, self.size)).unsqueeze(0) for im in im_crops], dim=0).float()
-        return im_batch
-
-
     def __call__(self, im_crops):
-        im_batch = self._preprocess(im_crops)
         with torch.no_grad():
-            im_batch = im_batch.to(self.device)
+            im_batch = []
+            for i, cropped_image in enumerate(im_crops):
+                # cv2.imshow("Image %d" % i, cropped_image)
+                # cv2.waitKey(1)
+
+                image = cv2.resize(cropped_image, self.size)
+                image = (image / 255.0).astype(np.float32)
+                image -= self.means
+                image /= self.stds
+                image = torch.from_numpy(image).permute(2, 0, 1).unsqueeze(0)
+                im_batch.append(image)
+            
+            im_batch = torch.cat(im_batch, dim=0)
+
+            if self.device != 'cpu':
+                im_batch = im_batch.to(self.device)
             features = self.net(im_batch)
-        return features.cpu().numpy()
+            print("Feautres:", features.shape)
+
+            if self.device.type != 'cpu':
+                features = features.cpu()
+            return features.numpy()
 
 
 if __name__ == '__main__':
