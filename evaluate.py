bae9d19d37e81d1733cfb9ef60e27b6d4bf86e8b
LimeOrangePie <ji5489@gmail.com>
2021-08-28 15:22:06 +0900

    [수정] 테스터기 코드 수정

--- evaluate.py
+++ evaluate.py
@@ -4,23 +4,23 @@ import numpy as np
 import time
 
 target_list = ['4p1b_01A2', '5p2b_01A1', '5p4b_01A2', '5p5b_03A1', '7p3b_02M']
-target_elapsed_time = []
-
-# for target in target_list:
-#     video_path = os.path.join('..', 'videos', target, target + '.m4v')
-#     gt_csv_path = os.path.join('..', 'videos', target, target + '.csv')
-
-#     if not (os.path.exists(video_path) and os.path.exists(gt_csv_path)):
-#         print('Missing video or ground truth csv for target {}'.format(target))
-#         continue
-
-#     begin = time.time()
-#     os.system('python solution %s %s' % (video_path, gt_csv_path))
-#     end = time.time()
-#     target_elapsed_time.append(end - begin)
 
 for i in range(len(target_list)):
-    target, elapsed_time = target_list[i], 0 # target_elapsed_time[i]
+    target = target_list[i]
+
+    video_path = os.path.join('..', 'videos', target, target + '.m4v')
+    sample_csv_path = os.path.join('..', 'videos', target, target + '.csv')
+
+    if not (os.path.exists(video_path) and os.path.exists(sample_csv_path)):
+        print('Missing video or ground truth csv for target {}'.format(target))
+        continue
+
+    begin = time.time()
+    os.system('python solution %s %s' % (video_path, sample_csv_path))
+    end = time.time()
+    
+    elapsed_time = end - begin
+
     gt_csv_path = os.path.join('..', 'videos', target, 'correct_%s.MP4.csv' % target)
     output_csv_path = os.path.join('outputs', target + '_out.csv')
 
@@ -45,7 +45,7 @@ for i in range(len(target_list)):
         is_correct = False
         for idx in range(output_csv_body.shape[0]):
             # print(int(output_csv_body[idx, 0]))
-            if frame - 2 <= output_csv_body[idx, 0] <= frame + 2:
+            if frame - 5 <= output_csv_body[idx, 0] <= frame + 5:
                 # print("%d Frame - Output: ", output_csv_body[idx, 1:], ", GT: ", items)
                 results.append((output_csv_body[idx, 1:] == items).astype(np.int32))
                 is_correct = True
